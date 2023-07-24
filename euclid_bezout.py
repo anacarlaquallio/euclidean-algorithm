a7eee14a7533187c5612a3c327dda26d5e3c8766
Ana Carla Quallio Rosa <92097155+anacarlaquallio@users.noreply.github.com>
2023-07-24 15:28:28 -0300

    Update euclid_bezout.py

--- euclid_bezout.py
+++ euclid_bezout.py
@@ -1,5 +1,11 @@
 DEBUG = False
 
+def eh_par(n):
+    if (n % 2) == 0:
+        return True
+    else:
+        return False
+
 def coeficientes(alfa, resto_lm, quociente, ab_rm):
     i = len(alfa) - 1
     y = (alfa[i]*ab_rm[0] - quociente[0])//resto_lm[0]
@@ -14,10 +20,10 @@ def calcula_alfa(n, resto_lm, quociente):
     alfa_i = [1]
 
     # define número de iterações
-    i = n // 2 if (n % 2 == 0) else (n - 1) // 2
+    i = n // 2 if eh_par(n) else (n - 1) // 2
 
     # define valores de k e l
-    k, l = (2, 0) if (n % 2 == 0) else (3, 1)
+    k, l = (2, 0) if eh_par(n) else (3, 1)
 
     linha = 1
     while (linha <= i):
@@ -28,10 +34,9 @@ def calcula_alfa(n, resto_lm, quociente):
         linha+=1
     return alfa_i
 
-# verifica se a*x + b*y = mdc(a, b)
 def verifica (ab, a, x, y):
     c = ab[0] * x + ab[1] * y
-    if (c == a):
+    if (c==a):
         return True
     else:
         return False
@@ -43,19 +48,20 @@ def mdc_extendido(a, b):
     resto_l = []
     quociente = []
     
-    # verificação casos triviais
+    # verificação triviais
     if (a == 0 and b == 0): return 0, 0, 0
-    elif (a == 0): return b, 0, 1
-    elif(b == 0): return 0, 0, 0
+    if (b == 0 and a != 0): return a, 1, 0
+    elif (a == 0 and b!= 0): return b, 0, 1
     elif(a == b): return a, 1, 0
     elif (a % b == 0): return b, 0, 1
 
     else:
+
         while (resto != 0):
             resto = a % b
             resto_l.append(resto)
             quociente.append(a//b)
-            a, b = b, resto
+            a, b = b, resto # atualização de valores
 
         resto_lm = metade_restos(resto_l, a)
         ab_lm = metade_restos(ab, a)
@@ -70,9 +76,12 @@ def mdc_extendido(a, b):
             print("Após a divisão por rn: ", ab_lm, resto_lm)
             print("Valores de alfa_i: ", alfa)
             print(verifica(ab, a, x, y), "\n")
+            
         return a, x, y
-    
-mdc, x, y = mdc_extendido(890, 376)
+
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+mdc, x, y = mdc_extendido(a, b)
 print ("MDC :", mdc)
 print("x: ", x)
 print("y: ", y)
